a51a3eee565b74dd99c729fce8dba54662d37b14
OBLAKOPOLYAKOV <misha.polyakov.96@gmail.com>
2022-05-21 19:24:12 +0300

    test_user_agent
    test_user_agent

---
+++ homework/test_user_agent_hm3_4.py
@@ -0,0 +1,33 @@
+import requests
+import pytest
+
+
+class TestUserAgent:
+    user_agents_and_expected = [
+        (
+            'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, '
+            'like Gecko) Version/4.0 Mobile Safari/534.30',
+            'Mobile', 'No', 'Android'),
+        (
+            'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) '
+            'CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1',
+            'Mobile', 'Chrome', 'iOS'),
+        ('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+         'Googlebot', 'Unknown', 'Unknown'),
+        (
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 '
+            'Safari/537.36 Edg/91.0.100.0',
+            'Web', 'Chrome', 'No'),
+        (
+            'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) '
+            'Version/13.0.3 Mobile/15E148 Safari/604.1',
+            'Mobile', 'No', 'iPhone')
+    ]
+
+    @pytest.mark.parametrize("user_agents, platform, browser, device", user_agents_and_expected)
+    def test_user_agent(self, user_agents, platform, browser, device):
+        url = "https://playground.learnqa.ru/ajax/api/user_agent_check"
+        response = requests.get(url, headers={"User-Agent": user_agents})
+        assert platform in response.text, f"Cannot find platform:'{platform}' in the response"
+        assert browser in response.text, f"Cannot find browser:'{browser}' in the response"
+        assert device in response.text, f"Cannot find device:'{device}' in the response"
